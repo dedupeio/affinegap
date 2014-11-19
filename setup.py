85a35dd02c4d562c443d735ca0c481a8d0cd2788
Forest Gregg <fgregg@uchicago.edu>
2014-11-19 14:08:55 -0600

    tests and setup.py

---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, Extension
+except ImportError :
+    raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
+
+# from Michael Hoffman's http://www.ebi.ac.uk/~hoffman/software/sunflower/
+
+class NumpyExtension(Extension):
+
+    def __init__(self, *args, **kwargs):
+        Extension.__init__(self, *args, **kwargs)
+
+        self._include_dirs = self.include_dirs
+        del self.include_dirs  # restore overwritten property
+
+    # warning: Extension is a classic class so it's not really read-only
+
+    @property
+    def include_dirs(self):
+        from numpy import get_include
+
+        return self._include_dirs + [get_include()]
+
+
+setup(
+    name='affinegap',
+    url='https://github.com/datamade/affinegap',
+    version='1.0',
+    description='A Cython implementation of the affine gap string distance',
+    packages=['affinegap'],
+    install_requires=['numpy'],
+    ext_modules=[NumpyExtension('affinegap.affinegap', ['affinegap/affinegap.c'])],
+    license='The MIT License: http://www.opensource.org/licenses/mit-license.php',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Cython', 
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis']
+    )
+
---
+++ tests/test_affinegap.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import unittest
+import affinegap
+import numpy
+
+class AffineGapTest(unittest.TestCase):
+    def setUp(self):
+        self.affineGapDistance = affinegap.affineGapDistance
+        self.normalizedAffineGapDistance = affinegap.normalizedAffineGapDistance
+    
+    def test_affine_gap_correctness(self):
+        assert self.affineGapDistance('a', u'b', -5, 5, 5, 1, 0.5) == 5
+        assert self.affineGapDistance('ab', 'cd', -5, 5, 5, 1, 0.5) == 10
+        assert self.affineGapDistance('ab', 'cde', -5, 5, 5, 1, 0.5) == 13
+        assert self.affineGapDistance('ab', u'cdë', -5, 5, 5, 1, 0.5) == 13
+        assert self.affineGapDistance('a', 'cde', -5, 5, 5, 1, 0.5) == 8.5
+        assert self.affineGapDistance('a', 'cd', -5, 5, 5, 1, 0.5) == 8
+        assert self.affineGapDistance('b', 'a', -5, 5, 5, 1, 0.5) == 5
+        assert self.affineGapDistance('a', 'a', -5, 5, 5, 1, 0.5) == -5
+        assert numpy.isnan(self.affineGapDistance('a', '', -5, 5, 5, 1, 0.5))
+        assert numpy.isnan(self.affineGapDistance('', '', -5, 5, 5, 1, 0.5))
+        assert self.affineGapDistance('aba', 'aaa', -5, 5, 5, 1, 0.5) == -5
+        assert self.affineGapDistance('aaa', 'aba', -5, 5, 5, 1, 0.5) == -5
+        assert self.affineGapDistance('aaa', 'aa', -5, 5, 5, 1, 0.5) == -7
+        assert self.affineGapDistance('aaa', 'a', -5, 5, 5, 1, 0.5) == -1.5
+        assert numpy.isnan(self.affineGapDistance('aaa', '', -5, 5, 5, 1, 0.5))
+        assert self.affineGapDistance('aaa', 'abba', -5, 5, 5, 1, 0.5) == 1
+    
+        def test_normalized_affine_gap_correctness(self):
+            assert numpy.isnan(self.normalizedAffineGapDistance('', '', -5, 5, 5, 1, 0.5))
+    
+if __name__ == "__main__":
+    unittest.main()
+
