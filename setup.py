3732f971d2117fb20a9a0242414373ef73ddbcc2
Forest Gregg <fgregg@uchicago.edu>
2015-10-16 13:03:01 -0500

    algorithm --> affinegap

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ except ImportError :
 setup(
     name='affinegap',
     url='https://github.com/datamade/affinegap',
-    version='1.6',
+    version='1.7',
     description='A Cython implementation of the affine gap string distance',
     packages=['affinegap'],
-    ext_modules=[Extension('affinegap.algorithm', ['affinegap/algorithm.c'])],
+    ext_modules=[Extension('affinegap.affinegap', ['affinegap/affinegap.c'])],
     license='The MIT License: http://www.opensource.org/licenses/mit-license.php',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
